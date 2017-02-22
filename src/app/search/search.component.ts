36226bb1ebc5f66147f16044a88538651250778f
Boopesh <boopeshmahendran@gmail.com>
2017-02-22 22:15:57 +0530

    Added Search profile functionality

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,22 @@
 import { Component} from '@angular/core';
+import { GithubService } from '../github.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [ GithubService ]
 })
-export class SearchComponent implements OnInit {
-  constructor() {
+export class SearchComponent {
+  private users[]: User; 
+  constructor(private githubService: GithubService) {
+  }
+  searchUsers(username: string) {
+      this.githubService.getUsers(username)
+                        .subscribe(res => {
+                              this.users = res.items;
+                              console.log(res);
+                         })
   }
 }
