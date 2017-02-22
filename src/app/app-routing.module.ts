d0b5fdf38d2f9f93269806aabc2141c3e00a547b
Boopesh <boopeshmahendran@gmail.com>
2017-02-22 20:41:09 +0530

    Added about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '',  component: SearchComponent },
+  { path: 'about',  component: AboutComponent },
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchComponent } from './search/search.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     NavbarComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
