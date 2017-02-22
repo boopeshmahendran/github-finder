5eba9a215bfeee1fa319e179e72d08e70b903f73
Boopesh <boopeshmahendran@gmail.com>
2017-02-22 22:56:08 +0530

    Updated user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,10 @@
 export class User {
+    name: string;
+    company: string;
     login: string;
     avatar_url: string;
     html_url: string;
+    followers: number;
+    following: number;
+    location: string;
 }
