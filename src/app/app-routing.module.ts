7e3b1def827d8fe503de4a9aa72ea3cca687c45d
Boopesh <boopeshmahendran@gmail.com>
2017-02-22 23:00:26 +0530

    Added routing support for ProfileComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '',  component: SearchComponent },
   { path: 'about',  component: AboutComponent },
+  { path: 'profile/:login',  component: ProfileComponent },
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
